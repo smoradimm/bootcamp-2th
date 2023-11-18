056300b846e0d34c6b9881d4d55c52613d537f4c
smoradimm <sm.moradi.somayeh@gmail.com>
2023-11-18 15:43:58 +0330

    session4-part of task

--- todo/todo.component.ts
+++ todo/todo.component.ts
@@ -1,13 +1,64 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { bindCallback } from 'rxjs';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class TodoComponent {
 
+export class AppComponent {
+
+  totalindex=0;  
+  tempnameoftask=""
+  temp:any;
+  
+  Menulistitem :any[]= [{
+          task:"" ,
+          isdo:false,
+          index:0 
+  }
+];
+  onHandledo(){
+    var Menulistitem1:any []=[{
+      task :"",
+      isdo:false,
+      index: Number
+    } ]   ;
+    console.log(this.temp);
+    
+    Menulistitem1= this.Menulistitem.filter(({task})=>task===this.temp);
+
+    Menulistitem1[0].isdo=true;            
+  }
+  onHandlemiit(){
+    this.Menulistitem= this.Menulistitem.filter(({task})=>task!==this.temp);
+
+  }
+  onHandleinsert(){
+      this.totalindex++;
+      var Menulistitem1={
+      task :this.temp,
+      isdo:false,
+      index:this.totalindex    
+    }    
+      this.Menulistitem.push(Menulistitem1);
+      console.log(Menulistitem1);
+    
+  }
+  onChangeTextin(e:any)
+  {  
+    console.log(e.target.value);
+    this.temp=e.target.value;
+  
+  }
+  
 }
+function Callback(): (value: any, index: number, array: any[]) => value is any {
+  throw new Error('Function not implemented.');
+}
+
